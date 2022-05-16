5a09a32a96b17fd364fdfad8f5ef6af2100d5a1e
RagDollPeach <kudrad26@gmail.com>
2022-05-16 13:55:18 +0300

    test app

---
+++ MyTestApplication/app/src/main/java/com/example/mytestapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mytestapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(view -> startCalculator());
+    }
+
+    public void startCalculator() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setComponent(new ComponentName("com.example.mycalculator",
+                "com.example.mycalculator.MainActivity"));
+        startActivity(intent);
+    }
+}
