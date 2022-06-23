fd16c03ad98873da61c83cc27b4496ae84de52a1
RagDollPeach <kudrad26@gmail.com>
2022-06-23 14:04:05 +0300

    weather app second commit

--- Kotlin/Weather/app/src/main/java/com/example/weather/MainActivity.kt
+++ Kotlin/Weather/app/src/main/java/com/example/weather/MainActivity.kt
@@ -14,21 +14,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val daysOfWeek = listOf(
+            resources.getString(R.string.app_name),
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+        )
+
         val note = Note("title", "name")
         val note1 = note.copy()
         val textToView = note1.title + " " + note1.note
 
         text = findViewById(R.id.text_view)
         button = findViewById(R.id.button)
-        val cobbledString = nextCycle(cycle(), textToView)
+
+        // жесткий вызов 3 методов, обычно так не делаю, но тут сплошной эксперемент так думаю простите
+        val cobbledString = nextCycle(cycle(testingWhen(daysOfWeek)), textToView)
+
         button.setOnClickListener { text.text = cobbledString }
+        findViewById<MaterialButton>(R.id.clear_button).setOnClickListener { text.text = "" }
     }
 
-    private fun cycle(): List<String> {
-        val daysOfWeek =
-            listOf(resources.getString(R.string.app_name), "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    private fun testingWhen(list : List<String>) : List<String> {
+        val translateDaysOfWeek = mutableListOf<String>()
+        for (s in list) {
+            when(s){
+                "Weather" -> {translateDaysOfWeek.add("Погода")}
+                "Tuesday" -> {translateDaysOfWeek.add("Вторник")}
+                "Wednesday" -> {translateDaysOfWeek.add("Среда")}
+                "Thursday" -> {translateDaysOfWeek.add("Четверг")}
+                "Friday" -> {translateDaysOfWeek.add("Пятница")}
+                "Saturday" -> {translateDaysOfWeek.add("Субота")}
+            }
+        }
+        return translateDaysOfWeek
+    }
+
+    private fun cycle(list : List<String>): List<String> {
         val daysWithIndex = mutableListOf<String>()
-        daysOfWeek.forEachIndexed { index, s ->
+        list.forEachIndexed { index, s ->
             val temp = "$s ${index + 1}"
             daysWithIndex.add(temp)
         }
